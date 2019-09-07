396d29397ae731469aa94d2377d9847f7c7df90b
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2019-09-07 15:23:51 +0700

    Initial commit

---
+++ app/src/main/java/com/rizkykhapidsyah/expimp_intent__kt/MoveActivity.kt
@@ -0,0 +1,12 @@
+package com.rizkykhapidsyah.expimp_intent__kt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
